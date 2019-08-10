dad82cfcb01ee0209e513a1237b45fb15e23099e
pradeep Edirisinghe <pradeep.edirisinghe@outlook.com>
2019-08-10 01:02:20 -0700

    Test code to check Config.properties and OR.properties files reading working okay or not.

---
+++ src/test/java/com/fdc/rough/TestProperties.java
@@ -0,0 +1,25 @@
+package com.fdc.rough;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println(System.getProperty("user.dir"));
+		Properties config = new Properties();
+		FileInputStream fis = new FileInputStream("C:\\ws\\DataDrivenFramework\\src\\test\\resources\\properties\\Config.properties");
+		config.load(fis);
+		
+		Properties OR = new Properties();
+		fis = new FileInputStream("C:\\ws\\DataDrivenFramework\\src\\test\\resources\\properties\\OR.properties");
+		OR.load(fis);
+		
+		System.out.println(config.getProperty("browser"));
+		System.out.println(OR.getProperty("bmlBtn"));
+
+	}
+
+}
